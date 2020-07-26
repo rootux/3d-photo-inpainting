673afa3eff4e184550ddc115a3986cbb9a4c426c
Gal Bracha <galbra@gmail.com>
2020-07-26 15:08:56 +0300

    Update download.sh
    Temp Fixed expired SSL certificates

--- download.sh
+++ download.sh
@@ -4,10 +4,10 @@ fb_status=$(wget --spider -S https://filebox.ece.vt.edu/ 2>&1 | grep  "HTTP/1.1 
 mkdir checkpoints
 
 echo "downloading from filebox ..."
-wget https://filebox.ece.vt.edu/~jbhuang/project/3DPhoto/model/color-model.pth
-wget https://filebox.ece.vt.edu/~jbhuang/project/3DPhoto/model/depth-model.pth
-wget https://filebox.ece.vt.edu/~jbhuang/project/3DPhoto/model/edge-model.pth
-wget https://filebox.ece.vt.edu/~jbhuang/project/3DPhoto/model/model.pt
+wget --no-check-certificate https://filebox.ece.vt.edu/~jbhuang/project/3DPhoto/model/color-model.pth
+wget --no-check-certificate https://filebox.ece.vt.edu/~jbhuang/project/3DPhoto/model/depth-model.pth
+wget --no-check-certificate https://filebox.ece.vt.edu/~jbhuang/project/3DPhoto/model/edge-model.pth
+wget --no-check-certificate https://filebox.ece.vt.edu/~jbhuang/project/3DPhoto/model/model.pt
 
 
 mv color-model.pth checkpoints/.
